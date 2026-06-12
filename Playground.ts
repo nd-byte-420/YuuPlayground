4dbcb863a7ba5d494e1cdb8425c8a230c77ded46
Nick L <lowmann@siu.edu>
2026-06-12 17:57:17 +0200

    Your commit message here

--- Playground.ts
+++ Playground.ts
@@ -166,7 +166,7 @@ async function start() {
 
   // playgroundDemos.spawnDissolveCubeBest(new Vector3(6, 1.5, 1));
 
-  playgroundDemos.spawnDissolveCubeRm(new Vector3(-6, 1.5, 3));
+  // playgroundDemos.spawnDissolveCubeRm(new Vector3(-6, 1.5, 3));
   playgroundDemos.spawnDissolveCubeRm1(new Vector3(6, 1.5, 3));
   playgroundDemos.spawnDissolveCube(new Vector3(6, 1.5, 4));
 
