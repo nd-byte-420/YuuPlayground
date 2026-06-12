79b973f41769277ebf3e81f5b3b6b274523a6ec8
Nick L <lowmann@siu.edu>
2026-06-12 15:54:12 +0200

    Your commit message here

--- PlaygroundLaex.ts
+++ PlaygroundLaex.ts
@@ -290,7 +290,7 @@ async function spawnDissolveCubeBest(pos: Vector3) {
 // create a cube and attach shadercode new/
 async function spawnDissolveCubeRm(pos: Vector3) {
 
-  const cube = spawnPrimitive.cube(pos, new Vector3(10,5,10), Quaternion.one, new Color(0.1,0.5,0.1), 1, true, 'Static', undefined);
+  const cube = spawnPrimitive.cube(pos, new Vector3(10,1,10), Quaternion.one, new Color(0.1,0.5,0.1), 1, true, 'Static', undefined);
 
   cube.collidable.set(false)
 
@@ -872,7 +872,7 @@ uniform float roughness = 0.5;
 // Proximity/Bubble Properties
 uniform vec3 player_position = vec3(0.0, 0.0, 0.0);
 uniform float bubble_inner_radius = 1.0; 
-uniform float bubble_outer_radius = 2.0; 
+uniform float bubble_outer_radius = 5.0; 
 
 // Internal Solid Cube Properties
 uniform vec4 grid_color : source_color = vec4(0.0, 1.0, 0.8, 1.0);
