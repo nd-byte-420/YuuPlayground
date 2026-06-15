2c2713ea96d685959d92a69062ccd5305f15e1a4
Nick L <lowmann@siu.edu>
2026-06-15 21:44:38 +0200

    Your commit message here

---
+++ CubeGun.ts
@@ -0,0 +1,90 @@
+import { Color } from "./Yuu API/Basic Types/Color";
+import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
+import { Vector3 } from "./Yuu API/Basic Types/Vector3";
+import { Controller } from "./Yuu API/Controller";
+import { Entity } from "./Yuu API/Entity";
+import { Player } from "./Yuu API/Player";
+import { Raycast } from "./Yuu API/Raycast";
+import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
+
+let cubeInventory = 0;
+const pickableCubes: Entity[] = [];
+
+export function initializeCubeGun() {
+    // Spawn initial pickable cubes for the player to use
+    for (let i = 0; i < 5; i++) {
+        const testCube = spawnPrimitive.cube(
+            new Vector3(0, 1 + i * 0.2, -2), // Position somewhat in front of the player
+            new Vector3(0.1, 0.1, 0.1),
+            Quaternion.one,
+            Color.blue,
+            1,
+            true, // Has collider
+            'Static', // Type
+            undefined
+        );
+        pickableCubes.push(testCube);
+    }
+
+    // Bind Right Grip for pickup
+    Controller.subscribe('rightGrip', 'Pressed', () => {
+        const rightHandPos = Player.rightHand.position.get();
+        const rightHandForward = Player.rightHand.forward.get();
+        
+        if (rightHandPos && rightHandForward) {
+            // Raycast properties: getEntity = true to identify the hit object
+            const hit = Raycast.directional(rightHandPos, rightHandForward, 100, { getEntity: true });
+            
+            if (hit && hit.entity) {
+                const index = pickableCubes.findIndex(c => c.nodeID === hit.entity!.nodeID);
+                if (index !== -1) {
+                    // Pick up cube
+                    const cubeToPickup = pickableCubes[index];
+                    cubeToPickup.destroy();
+                    pickableCubes.splice(index, 1);
+                    cubeInventory++;
+                    console.log(`Picked up a cube. Inventory: ${cubeInventory}`);
+                }
+            }
+        }
+    });
+
+    // Bind Right Trigger for firing/placing
+    Controller.subscribe('rightTrigger', 'Pressed', () => {
+        if (cubeInventory > 0) {
+            const rightHandPos = Player.rightHand.position.get();
+            const rightHandForward = Player.rightHand.forward.get();
+
+            if (rightHandPos && rightHandForward) {
+                const hit = Raycast.directional(rightHandPos, rightHandForward, 100, { getEntity: false });
+
+                if (hit) {
+                    // Offset slightly by normal to avoid clipping
+                    const offsetPos = hit.pos.add(hit.normal.multiply(0.05));
+                    
+                    // Snap to 0.1 grid
+                    const gridX = Math.round(offsetPos.x * 10) / 10;
+                    const gridY = Math.round(offsetPos.y * 10) / 10;
+                    const gridZ = Math.round(offsetPos.z * 10) / 10;
+                    const snappedPos = new Vector3(gridX, gridY, gridZ);
+
+                    // Spawn new cube
+                    const newCube = spawnPrimitive.cube(
+                        snappedPos,
+                        new Vector3(0.1, 0.1, 0.1),
+                        Quaternion.one,
+                        Color.white,
+                        1,
+                        true,
+                        'Static',
+                        undefined
+                    );
+
+                    pickableCubes.push(newCube);
+                    cubeInventory--;
+                    console.log(`Placed a cube. Inventory: ${cubeInventory}`);
+                }
+            }
+        }
+    });
+}
--- Playground.ts
+++ Playground.ts
@@ -2,7 +2,7 @@ import { registerStart } from "./Yuu API/RegisterStart";
 import { playgroundDemos } from "./PlaygroundLaex";
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { lexy } from "./PlaygroundLexy";
-import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
+import { initializeCubeGun } from "./CubeGun";
 
 registerStart(start);
 async function start() {
@@ -10,8 +10,6 @@ async function start() {
   playgroundDemos.spawnShaderSphere(new Vector3(-5, 2.5, -4));
 
   // playgroundDemos.spawnDissolveCubeRm2(new Vector3(5, 2.5, -4));
-  playgroundDemos.spawnDissolveCubeEfficient(new Vector3(4.5, 2.5, 0))
-  playgroundDemos.spawnDissolveCubeEfficient(new Vector3(-4.5, 2.5, 0))
   playgroundDemos.spawnDissolveCubeRm3(new Vector3(0, 2.5, -7))
   playgroundDemos.spawnDissolveCubeRm3(new Vector3(0, 2.5, 7))
 
@@ -19,12 +17,6 @@ async function start() {
   lexy.nissanGtr(new Vector3(25,0,0));
 
   lexy.sierPlane(new Vector3(0,0,-20));
-  // Come back to this to get water shader working
-  // playgroundDemos.spawnShaderSphere(new Vector3(-5, 2.5, -4));
-}
 
-// registerStart(start2);
-function start2() {
-  // const peer = new Peer();
-  console.log(Godot.networking.rtcPeer.create());
+  initializeCubeGun();
 }
