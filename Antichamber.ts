ee6f3ebc5c92bb06467bd3fba0303479683a60b5
Nick L <lowmann@siu.edu>
2026-06-15 23:55:34 +0200

    Your commit message here

--- Antichamber.ts
+++ Antichamber.ts
@@ -9,6 +9,7 @@ import { Player } from "./Yuu API/Player";
 import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 import { Texture } from "./Yuu API/Texture";
 import { CubeEntity } from "./CubeGun";
+import { Events } from "./Yuu API/Events";
 
 export const antichamber = {
   spawnDoor,
@@ -21,8 +22,22 @@ async function spawnDoor(pos: Vector3) {
 
   cube.collidable.set(true);
 
+  Events.onPhysicsUpdate(() => {
+    if (cube.exists()) {
+      cube.rot = Quaternion.one;
+      
+      const curPos = cube.pos;
+      cube.pos = new Vector3(pos.x, curPos.y, pos.z);
+      
+      const vel = cube.velocity.get();
+      if (vel) {
+        cube.velocity.set(new Vector3(0, vel.y, 0));
+      }
+    }
+  });
+
   const supportPos = new Vector3(pos.x, pos.y + 2.5, pos.z);
-  const supportCube = new CubeEntity(supportPos, true, new Vector3(1, 5, 1), new Color(0.5, 0.5, 0.5), 'Physics');
+  const supportCube = new CubeEntity(supportPos, true, new Vector3(0.1,0.1,0.1), new Color(0.5, 0.5, 0.5), 'Physics');
   const support = supportCube.entity;
   support.collidable.set(true);
 }
