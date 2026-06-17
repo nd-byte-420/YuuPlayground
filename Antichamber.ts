d378a03c70c477eeb1b1798f168b126c8a1511de
Nick L <lowmann@siu.edu>
2026-06-17 21:22:16 +0200

    Your commit message here

--- Antichamber.ts
+++ Antichamber.ts
@@ -20,7 +20,8 @@ async function spawnDoor(pos: Vector3) {
   const doorPos = new Vector3(pos.x, pos.y + 2 * 2, pos.z);
   // const cube = spawnPrimitive.cube(doorPos, new Vector3(5, 5, 0.1), Quaternion.one, new Color(0.1, 0.5, 0.1), 1, true, 'Physics', undefined);
 
-  const door = spawnPrimitive.door2(doorPos, new Vector3(0.05, 1, 1), Quaternion.one, new Color(0.1, 0.5, 0.1), 1, true, 'Physics', undefined);
+  // door can still be glitched through with
+  const door = spawnPrimitive.door3(doorPos, new Vector3(1, 1, 1), Quaternion.one, new Color(0.1, 0.5, 0.1), 1, true, 'Physics', undefined);
   const nodeId = door.mesh.nodeID ?? -1;
   Godot.shader.applyToMesh(nodeId, doorShader4);
 
@@ -31,7 +32,7 @@ async function spawnDoor(pos: Vector3) {
   const triggerEntity = new Entity(triggerPos, Quaternion.one, new Vector3(0.5, 0.5, 4), undefined, 'Static');
 
   triggerEntity.trigger.initialize(new Vector3(0.5, 0.5, 4));
-  triggerEntity.trigger.setVisible(true, Color.red);
+  triggerEntity.trigger.setVisible(true, Color.blue);
 
   triggerEntity.trigger.setOccupiedFunction(() => {
     isGravityReversed = true;
