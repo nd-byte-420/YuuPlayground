483413a64c054af03403ae423186445f23c3a31a
Nick L <lowmann@siu.edu>
2026-06-17 12:31:04 +0200

    fix shader

--- Scene.ts
+++ Scene.ts
@@ -9,6 +9,20 @@ export const scene = {
 
 // create a cube and attach shadercode new/
 async function spawnChamber() {
-    const chamber = spawnPrimitive.chamber(new Vector3(0,0,0), new Vector3(1,1,1), Quaternion.one, new Color(0.1, 0.5, 0.1), 1, true, 'Physics', undefined);
+  const chamber = spawnPrimitive.chamber(new Vector3(0, 0, 0), new Vector3(2, 2, 2), Quaternion.one, new Color(0.1, 0.5, 0.1), 1, true, 'Physics', undefined);
+
+  const nodeId = chamber.mesh.nodeID ?? -1;
+  Godot.shader.applyToMesh(nodeId, chamberShader);
 }
+
+const chamberShader = `
+shader_type spatial;
+render_mode cull_disabled;
+
+uniform vec4 wall_color : source_color = vec4(0.8, 0.8, 0.8, 1.0);
+
+void fragment() {
+    ALBEDO = wall_color.rgb;
+}
+`
 
